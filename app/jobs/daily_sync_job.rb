f421500d02e7f1d991a4935d9741879ba475c266
David Buckley <david@davidbuckley.ca>
2025-02-17 10:10:29 -0500

    later

--- app/jobs/daily_sync_job.rb
+++ app/jobs/daily_sync_job.rb
@@ -1,10 +1,9 @@
 class DailySyncJob < ApplicationJob
   def perform
-    # TODO: These should probably perform_later
     User.all.each do |user|
-      Import::FetchWithingsJob.perform_now(user.id) if user.withings_refresh_token?
+      Import::FetchWithingsJob.perform_later(user.id) if user.withings_refresh_token?
     end
 
-    Import::ScanLoseitEmailsJob.perform_now
+    Import::ScanLoseitEmailsJob.perform_later
   end
 end
