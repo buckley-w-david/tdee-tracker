3253abb4b5098adfe0d62e9160a337b8bc9d40a0
David Buckley <david@davidbuckley.ca>
2025-03-16 11:36:02 -0400

    improve routine editing UI

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -2,6 +2,7 @@ const defaultTheme = require('tailwindcss/defaultTheme')
 
 module.exports = {
   prefix: 'tw-',
+  important: "#app",
   content: [
     './public/*.html',
     './app/helpers/**/*.rb',
